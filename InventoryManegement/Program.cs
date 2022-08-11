b9a269dba4e13830e7ca35f458c55f507ec9341b
dabhade904 <dabhade904@gmail.com>
2022-08-11 12:42:16 +0530

    Added : read json file and calculating inventory

---
+++ InventoryManegement/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InventoryManegement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FetchInventoryManegement obj = new FetchInventoryManegement();
+            string filePath = "C:\\Users\\sir\\Desktop\\dotNet\\InventoryManegement\\InventoryManegement\\Data.json";
+            Rice data = obj.Read(filePath);
+            // Console.WriteLine(data);
+            for (int i = 0; i < data.typeOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typeOfRice[i].name);
+                Console.WriteLine(data.typeOfRice[i].weight);
+                Console.WriteLine(data.typeOfRice[i].price);
+                Console.WriteLine("------------------------");
+            }
+
+            for (int i = 0; i < data.typeOfWheats.Count; i++)
+            {
+                Console.WriteLine(data.typeOfWheats[i].name);
+                Console.WriteLine(data.typeOfWheats[i].weight);
+                Console.WriteLine(data.typeOfWheats[i].price);
+                Console.WriteLine("------------------------");
+            }
+
+            for (int i = 0; i < data.typeOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typeOfPulses[i].name);
+                Console.WriteLine(data.typeOfPulses[i].weight);
+                Console.WriteLine(data.typeOfPulses[i].price);
+                Console.WriteLine("------------------------");
+            }
+        }
+    }
+}
